47ad8155ffd23291f1e6448cafecb88104f79b71
rajangyawali <rgkg2@umsystem.edu>
2024-06-17 18:53:47 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My Personal Website : https://www.rajangyawali.github.io
---
+++ others/extra.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#fb").click(function(){
+		window.location = "https://www.facebook.com/rajangyawali93";
+	});
+	$("#insta").click(function(){
+		window.location = "https://www.instagram.com/rajan.gyawali/";
+	});
+	$("#twitter").click(function(){
+		window.location = "https://twitter.com/rajangyawali93";
+	});
+	$("#linked").click(function(){
+		window.location = "https://np.linkedin.com/in/rajangyawali";
+	});
+	$("#git").click(function(){
+		window.location = "https://github.com/rajangyawali";
+	});
+});
